527927e80aa71615ff4a9a8bc7b2b4f293684ac1
azy64 <allysaidi64@gmail.com>
2021-12-21 12:29:07 +0100

    fix linter errors

--- person.rb
+++ person.rb
@@ -1,20 +1,23 @@
 class Person
-    attr_accessor :id, :name, :age
-    def initialize(age, name = 'unknown', parent_permission = true)
-        @age = age
-        @name = name
-        @parent_permission = parent_permission
-    end
-    def can_use_services?
-        is_of_age? && @parent_permission
-    end
-    def is_of_age?
-        @age >= 18
-    end
-    private :is_of_age?
+  attr_accessor :id, :name, :age
+
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? && @parent_permission
+  end
+
+  def of_age?
+    @age >= 18
+  end
+  private :is_of_age?
 end
 
-p = Person.new(21,"ALLY",true)
-puts(p.name+" -- #{p.age}")
-p.name=("SAIDI ALLY")
+p = Person.new(21, 'ALLY', true)
+puts(p.name + " -- #{p.age}")
+p.name = ('SAIDI ALLY')
 puts(p.can_use_services?)
