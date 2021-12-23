6af19384591041dd929521ba89d984062bbadf86
azy64 <allysaidi64@gmail.com>
2021-12-23 20:49:57 +0100

    add the main method

--- person.rb
+++ person.rb
@@ -5,10 +5,10 @@ class Person
   attr_reader :id, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
-    @id = Random.rand(1..1000)
     @corrector = Corrector.new
     @rentals = []
   end
@@ -29,10 +29,5 @@ class Person
   def of_age?
     @age >= 18
   end
-  private :is_of_age?
+  private :of_age?
 end
-
-p = Person.new(21, 'ALLY', true)
-puts(p.name + " -- #{p.age}")
-p.name = ('SAIDI ALLY')
-puts(p.can_use_services?)
--- student.rb
+++ student.rb
@@ -3,8 +3,10 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, name, parent_permission)
+    super(age)
+    @name = name
+    @parent_permission = parent_permission
     @classroom = classroom
     @classroom.students.push(self) unless @classroom.students.include?(self)
   end
