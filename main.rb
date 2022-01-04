ef49c9a927bd741a251e1e9bb6050824c1a20893
victor-chukwu <okenwa1993@gmail.com>
2022-01-04 10:43:19 +0100

    Created new class to manage person, student and teacher methods

--- main.rb
+++ main.rb
@@ -1,8 +1,7 @@
 #! /usr/bin/env ruby
 
 require './book'
-require './student'
-require './teacher'
+require_relative 'manage_people'
 require './classroom'
 require './rental'
 require './options_message'
@@ -16,56 +15,6 @@ def all_books(books)
     i += 1
   end
   puts ''
-end
-
-def all_person(people)
-  i = 0
-  puts 'Here is the list of person:'
-  while i < people.length
-    person = people[i]
-    puts "[#{person.class}]Name:  #{person&.name&.capitalize}, ID: #{person&.id}, Age: #{person&.age} "
-    i += 1
-  end
-  puts ''
-end
-
-def create_student
-  print 'Age:'
-  age = gets.chomp.to_i
-  print 'Name:'
-  name = gets.chomp
-  print 'Has parents permission? [Y/N]:'
-  permit = gets.chomp.upcase == 'Y'
-  classroom = Classroom.new('L1')
-  student = Student.new(classroom, age, name, permit)
-  student.validate_name
-  student
-end
-
-def create_teacher
-  print 'Age:'
-  age = gets.chomp.to_i
-  print 'Name:'
-  name = gets.chomp
-  print 'Specialization:'
-  specialization = gets.chomp
-  teacher = Teacher.new(specialization, age, name)
-  teacher.validate_name
-  teacher
-end
-
-def create_person(people)
-  print 'Do you want to create a student (1) or a teacher (2) [Input the number]:'
-  choice = gets.chomp.to_i
-  case choice
-  when 1
-    student = create_student
-    people.push(student)
-  when 2
-    teacher = create_teacher
-    people.push(teacher)
-  end
-  puts 'Person created successfully'
 end
 
 def create_book(books)
@@ -124,9 +73,9 @@ def options(response, books, people, rentals)
   when 1
     all_books(books)
   when 2
-    all_person(people)
+    ManagePeople.all_person(people)
   when 3
-    create_person(people)
+    ManagePeople.create_person(people)
   when 4
     create_book(books)
   when 5
@@ -142,7 +91,7 @@ def main
   people = []
   rentals = []
   while response != 7
-    welcome_message
+    option_message
     print 'Please enter your choice:'
     response = gets.chomp.to_i
     options(response, books, people, rentals)
