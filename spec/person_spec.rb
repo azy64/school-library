9a6c7e49300ef9386de63f3e2ae390ff7f4406f1
victor-chukwu <okenwa1993@gmail.com>
2022-01-06 11:55:45 +0100

    Fixed linters

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,7 +2,7 @@ require './person'
 
 describe 'Person' do
   it 'should initialize correctly' do
-    pw = Person.new( 10, 'victor')
+    pw = Person.new(10, 'victor')
     expect(pw.name).to eql 'victor'
     expect(pw.age).to eql 10
   end
